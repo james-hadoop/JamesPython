c4cea7c1de9afe757ba32b9776cca916e2b3ab7f
james <james@JamesUbuntu>
2019-10-14 07:58:18 +0800

    recommit

--- james_sklearn/test.py
+++ james_sklearn/test.py
@@ -16,19 +16,19 @@ feature = [[float(x) for x in row[:]] for row in data]
 #调用kmeans类
 clf = KMeans(n_clusters=9)
 s = clf.fit(feature)
-print s
+print(s)
 
 #9个中心
-print clf.cluster_centers_
+print(clf.cluster_centers_)
 
 #每个样本所属的簇
-print clf.labels_
+print(clf.labels_)
 
 #用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
-print clf.inertia_
+print(clf.inertia_)
 
 #进行预测
-print clf.predict(feature)
+print(clf.predict(feature))
 
 #保存模型
 joblib.dump(clf, '/Users/qjiang/Desktop/km.pkl')
