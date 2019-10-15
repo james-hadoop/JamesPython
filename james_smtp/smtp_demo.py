ee5884489843eba2a9e2309b0acde8cc05eda28c
james <james_hadoop@163.com>
2019-10-15 08:54:49 +0800

    add send_email

--- james_smtp/smtp_demo.py
+++ james_smtp/smtp_demo.py
@@ -1,11 +1,23 @@
 import smtplib
-import string
 
-HOST='smtp.gmail.com'
-SUBJECT='James SMTP Demo'
-TO='james@126.com'
-FROM='james@126.com'
-text='Python rules them all!'
-BODY=string.join((
-
+HOST = 'smtp@163.com'
+SUBJECT = 'James SMTP Demo'
+TO = 'jamesqjiang@163.com'
+FROM = 'jamesqjiang@163.com'
+text = 'Python rules them all!'
+BODY = "\r\n".join((
+    "From: %s" % FROM,
+    "To: %s" % TO,
+    "Subject: %s" % SUBJECT,
+    "",
+    text
 ))
+
+print(BODY)
+
+server = smtplib.SMTP()
+server.connect(HOST, "25")
+server.starttls()
+server.login("jamesqjiang@163.com", "james123")
+server.sendmail(FROM, [TO], BODY)
+server.quit()
