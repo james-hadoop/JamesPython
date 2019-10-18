b8ba6128f0d5e4f4cd928a0e18ce2c1aa841c844
james <james_hadoop@163.com>
2019-10-18 09:19:13 +0800

    add json

---
+++ james_json/json_demo.py
@@ -0,0 +1,20 @@
+import json
+
+f = open('demo.json', 'r')
+
+data = f.read()
+json_data = json.loads(data)
+
+print(json_data['name'])
+f.close()
+
+dic = {
+    "name": "james",
+    "age": 18
+}
+
+data = json.dumps(dic)
+
+f = open('json_demo2.json', 'w')
+f.write(data)
+f.close()
