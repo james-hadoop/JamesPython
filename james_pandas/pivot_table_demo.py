e09573e09bbd7ff11e5904ff3a0b6154232d5125
james <james_hadoop@163.com>
2019-10-23 15:37:45 +0800

    add matplotlib

--- james_pandas/pivot_table_demo.py
+++ james_pandas/pivot_table_demo.py
@@ -12,6 +12,8 @@ df = pd.read_csv('../_data/account_top200.csv')
 
 df = df[df.puin.isin(['2713129639'])]
 
-table = df.pivot_table(index='week_of_year', columns='input_week_day')
+data = df[['s_input_date', 'week_of_year', 'input_week_day', 'cnt']]
+
+table = data.pivot_table(index='week_of_year', columns='input_week_day')
 
 print(table)
