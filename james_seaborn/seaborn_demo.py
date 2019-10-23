e4a521a2f75d2726772012b30c127f8e444dcb3a
james <james@JamesUbuntu>
2019-10-24 00:18:50 +0800

    add seaborn

--- james_seaborn/seaborn_demo.py
+++ james_seaborn/seaborn_demo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option('display.max_columns', 1000)
 
@@ -9,13 +11,14 @@ pd.set_option('display.max_colwidth', 1000)
 pd.set_option('display.max_rows', None)
 
 df = pd.read_csv('../_data/account_top200.csv')
+# print(df)
 
 df = df[df.puin.isin(['2713129639'])]
 df = df[df['week_of_year'] > 33]
 df = df[df['week_of_year'] < 43]
+df = df.sort_values(axis=0, by=['week_of_year', 'input_week_day'], ascending=True)
+# print(df)
+df = df[['input_week_day', 'cnt']]
+# print(data)
 
-data = df[['s_input_date', 'week_of_year', 'input_week_day', 'cnt']]
-
-table = data.pivot_table(index='week_of_year', columns='input_week_day')
-
-print(table)
+sns.boxplot(x='input_week_day', y='cnt', data=df)
