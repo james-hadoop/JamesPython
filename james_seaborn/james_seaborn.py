27d32a525994e4cb7135242bac1984c70242e2c0
james <james@JamesUbuntu>
2019-10-24 00:35:59 +0800

    add seaborn

---
+++ james_seaborn/james_seaborn.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(color_codes=True)
+
+x = np.random.normal(size=100)
+# 绘制直方图.kde即kernel density estimate，用于控制是否绘制核密度估计
+sns.distplot(x, kde=True)
+
+mean, cov = [0, 3], [(1, .5), (.5, 1)]
+# mean是多维分布的均值；cov是协方差矩阵。注意：协方差矩阵必须是对称的且需为半正定矩阵；
+data = np.random.multivariate_normal(mean, cov, 200)
+df = pd.DataFrame(data, columns=["x", "y"])
+# 创建密度图
+sns.jointplot(x="x", y="y", data=df)
+plt.show()
--- james_seaborn/seaborn_demo.py
+++ james_seaborn/seaborn_demo.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', 1000)
 
@@ -18,7 +18,12 @@ df = df[df['week_of_year'] > 33]
 df = df[df['week_of_year'] < 43]
 df = df.sort_values(axis=0, by=['week_of_year', 'input_week_day'], ascending=True)
 # print(df)
-df = df[['input_week_day', 'cnt']]
-# print(data)
+data = df[['input_week_day', 'cnt']]
+print(data)
+# print(data['cnt'].describe())
 
-sns.boxplot(x='input_week_day', y='cnt', data=df)
+sns.boxplot(x='input_week_day', y='cnt', data=data)
+plt.show()
+
+data = data[data['input_week_day'] == 1]
+print(data['cnt'].describe())
