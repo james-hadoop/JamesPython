0f57cae20f62af3272b70e414ce8227cc1b18f10
james <james@JamesUbuntu>
2019-12-01 23:20:32 +0800

    success send_email_with_excel

--- james_smtp/send_email_with_excel.py
+++ james_smtp/send_email_with_excel.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.header import Header
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -14,20 +15,27 @@ receivers = ['jamesqjiang@163.com']
 content = '我用Python'
 title = 'send_email_with_excel'
 csv_file_path = "/home/james/桌面/_CURRENT_WORK/_爬虫数据/shenzhen_201911202222.csv"
+csv_file_name = "shenzhen_201911202222.csv"
 
 msg = MIMEMultipart()
-attachment = MIMEText(open(csv_file_path, 'rb').read(), 'base64',
-                      'utf-8')
-attachment["Content-Type"] = 'application/octet-stream'
-# 生成附件的名称
-attachment[
-    "Content-Disposition"] = 'attachment; filename=' + csv_file_path
-# 将附件内容插入邮件中
-msg.attach(attachment)
-
+msg.attach(MIMEText("send_email_with_excel"))
 msg['Subject'] = Header(title, 'utf-8')  # subject
-msg['From'] = sender
-msg['To'] = receivers
+# msg['From'] = sender
+# msg['To'] = receivers
+
+xlsx = MIMEText(open(csv_file_path, 'rb').read(), 'base64', 'utf-8')
+xlsx["Content-Type"] = 'application/octet-stream'
+xlsx.add_header('Content-Disposition', 'attachment', filename=csv_file_name)
+msg.attach(xlsx)
+
+# attachment = MIMEText(open(csv_file_path, 'rb').read(), 'base64', 'utf-8')
+# attachment["Content-Type"] = 'application/octet-stream'
+# # 生成附件的名称
+# attachment[
+#     "Content-Disposition"] = 'attachment; filename=' + csv_file_name
+# # 将附件内容插入邮件中
+# msg.attach(attachment)
+
 
 smtpObj = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口一般是465
 smtpObj.login(mail_user, mail_pass)  # 登录验证
