64bf1f86269910c8103ee26280b62a687fc7a557
james <james@JamesUbuntu>
2020-02-16 19:05:28 +0800

    add james_redis

--- james_http/PostDemo.py
+++ james_http/PostDemo.py
@@ -1,8 +1,8 @@
 import json
-import urllib2
+import urllib
 
 url = 'http://localhost:8088/v1/service/user/getVerifyCode?hUserPhoneNr=15821615886'
-req = urllib2.Request(url)
-res = urllib2.urlopen(req)
+req = urllib.Request(url)
+res = urllib.urlopen(req)
 res = res.read()
 print(res)
