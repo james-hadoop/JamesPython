3a730db8f0a787cc33f186ed384737e615820d0a
james <james@JamesUbuntu>
2020-04-09 22:18:04 +0800

    add james_code

--- james_code/solution.py
+++ james_code/solution.py
@@ -5,7 +5,7 @@
 
 class Solution:
     """
-        https://www.nowcoder.com/practice/c6c7742f5ba7442aada113136ddea0c3?tpId=13&tqId=11160&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+        [!斐波那契数列](https://www.nowcoder.com/practice/c6c7742f5ba7442aada113136ddea0c3?tpId=13&tqId=11160&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking)
     """
 
     def fibonacci(self, n):
@@ -33,7 +33,29 @@ class Solution:
         return ret
 
     """
-        https://www.nowcoder.com/practice/d8b6b4358f774294a89de2a6ac4d9337?tpId=13&tqId=11169&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+        [!跳台阶](https://www.nowcoder.com/practice/8c82a5b80378478f9484d87d1c5f12a4?tpId=13&tqId=11161&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking)
+    """
+
+    def jump_floor(self, number):
+        if number < 1:
+            return 0
+        if number == 1:
+            return 1
+        if number == 2:
+            return 2
+
+        ret = 0;
+        a = 1
+        b = 2
+        for i in range(3, number + 1):
+            ret = a + b
+            a = b
+            b = ret
+
+        return ret
+
+    """
+        [!合并两个排序的链表](https://www.nowcoder.com/practice/d8b6b4358f774294a89de2a6ac4d9337?tpId=13&tqId=11169&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking)
     """
 
     def merge_sort_link(self, p_head1, p_head2):
