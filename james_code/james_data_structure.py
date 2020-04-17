ce231bc298c79ce3e7dd6b5e4e2ba00eb21f77b7
jamesqjiang <jamesqjiang@tencent.com>
2020-04-18 00:36:05 +0800

    upadte

--- james_code/james_data_structure.py
+++ james_code/james_data_structure.py
@@ -1,3 +1,5 @@
+import string
+
 from pythonds.basic import Stack
 
 
@@ -64,6 +66,84 @@ def parChecker(symbolString):
         return True
     else:
         return False
+
+
+# 3-6 将十进制数转换成任意进制数
+def baseConverter(decNumber, base):
+    digits = "0123456789ABCDEF"
+
+    remStack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % base
+        remStack.push(rem)
+        decNumber = decNumber // base
+
+    newString = ""
+    while not remStack.isEmpty():
+        newString = newString + digits[remStack.pop()]
+
+    return newString
+
+
+# 3-7 从中序表达式到后序表达式的转换
+def infixToPostfix(infixexp):
+    prec_dict = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
+
+    opStack = Stack()
+    postfixList = []
+
+    tokenList = infixexp.split()
+
+    for token in tokenList:
+        if token in string.ascii_uppercase:
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and (prec_dict[opStack.peek()] >= prec_dict[token]):
+                postfixList.append(opStack.pop())
+
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+
+    return " ".join(postfixList)
+
+
+# 3-8 实现后序表达式的计算
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+
+    return operandStack.pop()
 
 
 def main():
