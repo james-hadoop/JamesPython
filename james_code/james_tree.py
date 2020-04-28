bc4a03e4981e00b594dd53c5c40e0660c5a89540
james <james@JamesUbuntu>
2020-04-29 07:48:12 +0800

    add BinaryHeap

--- james_code/james_tree.py
+++ james_code/james_tree.py
@@ -1,4 +1,6 @@
 # 6-5 BinaryTree类
+from tokenize import Operator
+
 from pythonds import Stack
 
 
@@ -73,6 +75,115 @@ def buildParseTree(fpexp):
             raise ValueError("Unknown Operator: " + i)
 
     return eTree
+
+
+# 6-10 计算二叉解析树的递归函数
+def evaluate(parseTree):
+    opers = {'+': Operator.add, '-': Operator.sub, '*': Operator.mul, '/': Operator.truediv}
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChile()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+
+# 6-11 将前序遍历算法实现为外部函数
+def preorder1(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder1(tree.getLeftChild())
+        preorder1(tree.getRightChild())
+
+
+# 6-12 将前序遍历算法实现为BinaryTree类的方法
+def preorder(self):
+    print(self.key)
+    if self.leftChild:
+        self.leftChild.preorder()
+    if self.rightChild:
+        self.rightChild.preorder()
+
+
+# 6-13 后序遍历
+def postorder(tree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+
+# 6-14 后续遍历求值函数
+def postordereval(tree):
+    opers = {'+': Operator.add, '-': Operator.sub, '*': Operator.mul, '/': Operator.truediv}
+
+    res1 = None
+    res2 = None
+
+    if tree:
+        res1 = postordereval(tree.getLeftChild())
+        res2 = postordereval(tree.getRightChild())
+
+        if res1 and res2:
+            return opers[tree.getRootVal()](res1, res2)
+        else:
+            return tree.getRootVal()
+
+
+# 6-15  中序遍历
+def inorder(tree):
+    if tree != None:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
+
+# 6-17 二叉堆
+class BinaryHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i // 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize += 1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def buildHeap(self, alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist
+        while (i > 0):
+            self.percDown(i)
+            i = i = 1
 
 
 def main():
