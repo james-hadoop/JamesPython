fbe75a2fdd924c35fd9290ce6476999b76b037c2
jamesqjiang <jamesqjiang@tencent.com>
2020-06-09 11:35:11 +0800

    add james_hive_lineage_parser

--- james_basic/james_test.py
+++ james_basic/james_test.py
@@ -1,8 +1,12 @@
 import base64
 import re
 
-expression = 'KChrYW5kaWFuX29kc19hbGxfY29tbWVudF9jb216YW5fZC5mdGltZSA9IDIwMjAwNjA3KSBhbmQg\r\na2FuZGlhbl9vZHNfYWxsX2NvbW1lbnRfY29temFuX2Qucm93a2V5IGlzIG5vdCBudWxsKQ=='
-clean_expression = re.sub('[\s+]', '', expression)
+# expression = 'KChrYW5kaWFuX29kc19hbGxfY29tbWVudF9jb216YW5fZC5mdGltZSA9IDIwMjAwNjA3KSBhbmQg\r\na2FuZGlhbl9vZHNfYWxsX2NvbW1lbnRfY29temFuX2Qucm93a2V5IGlzIG5vdCBudWxsKQ=='
+# clean_expression = re.sub('[\s+]', '', expression)
+#
+# bb = base64.decodebytes(clean_expression.encode())
+# print(bb.decode("utf-8"))
 
-bb = base64.decodebytes(clean_expression.encode())
-print(bb.decode("utf-8"))
+str = 'sng_mediaaccount_app.kandian_video_medium_full_info_d.kddaily_expose'
+src_table_name = str.rsplit('.', 1)[0]
+print(f"src_table_name={src_table_name}")
---
+++ james_hive_lineage/james_hive_lineage_parser.py
@@ -0,0 +1,144 @@
+import datetime
+
+import configobj
+import pymysql
+import json
+import os
+import re
+import base64
+
+from pyecharts import options as opts
+from pyecharts.charts import Graph
+
+from py_kd_data_common_proj.ds_utils.ds_pymysql_util import fetch_all, execute_sql
+
+config_path = os.getcwd() + '/../james_config/james_config.conf'
+CO = configobj.ConfigObj(config_path)
+
+
+def write_to_table(DB_COR, DB_CONN):
+    update_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    update_name = 'jamesqjiang'
+    value = 'i am a value@' + update_time
+    ext = '{}'
+
+    sql_insert = "'%s', '%s', '%s', '%s'" % (
+        update_time, update_name, value, ext)
+
+    execute_sql(DB_COR, DB_CONN,
+                "insert into t_ds_demo (update_time, update_name, value, ext) values (%s)" % sql_insert)
+
+
+def read_from_table(DB_COR):
+    sql_select = "SELECT lineage_str FROM developer.hive_lineage_log where id=15;"
+    results = fetch_all(DB_COR,
+                        sql_select)
+    return results
+
+
+def process_lineage_hook_info(lineage):
+    clean_lineage = re.sub('[\s+]', '', lineage)
+    data_dict = json.loads(clean_lineage)
+
+    _target_table = '_NULL'
+    node_set = set()
+    nodes_data = []
+    links_data = []
+    flag = True
+
+    # 所有顶点装入字典
+    vertices = data_dict["vertices"]
+    vertices_dict = {}
+    for elem in vertices:
+        key = elem["id"]
+        type = elem["vertexType"]
+        value = elem["vertexId"]
+        vertices_dict[key] = (type, value)
+
+    # 解析边的关系
+    i = 100
+    j = 100
+    edges = data_dict["edges"]
+    for elem in edges:
+        # print(elem)
+        sources = elem["sources"]
+        targets = elem["targets"]
+        edgeType = elem["edgeType"]
+        if "expression" in elem:
+            expression = elem["expression"]
+        if edgeType == "PROJECTION":
+            for source in sources:
+                if vertices_dict[source][0] == "COLUMN":
+                    origin_column_name = vertices_dict[source][1]
+                elif vertices_dict[source][0] == "TABLE":
+                    origin_column_name = vertices_dict[source][1] + "." + expression
+
+                for target in targets:
+                    if vertices_dict[target][0] == "COLUMN":
+                        dest_column_name = vertices_dict[target][1]
+                    elif vertices_dict[target][0] == "TABLE":
+                        dest_column_name = vertices_dict[target][1] + "." + expression
+
+                    str_expression = base64.decodebytes(expression.encode()).decode('utf-8')
+                    ## 打印点边关系
+                    print(
+                        f"{origin_column_name} -> {dest_column_name} ->@ {str_expression} << {origin_column_name.rsplit('.', 1)[0]} -> {dest_column_name.rsplit('.', 1)[0]}")
+
+                    ## 画图功能，图中添加关系
+                    if origin_column_name not in node_set:
+                        nodes_data.append(
+                            opts.GraphNode(name=origin_column_name, symbol_size=10, is_fixed=True, x=100, y=100 + i))
+                        node_set.add(origin_column_name)
+                        i += 2000
+
+                    if dest_column_name not in node_set:
+                        nodes_data.append(
+                            opts.GraphNode(name=dest_column_name, symbol_size=10, is_fixed=True, x=20000, y=100 + j))
+                        node_set.add(dest_column_name)
+                        j += 2000
+
+                    links_data.append(
+                        opts.GraphLink(source=origin_column_name, target=dest_column_name,
+                                       value=expression))
+
+    # 画图
+    c = (
+        Graph(init_opts=opts.InitOpts(width="1600px", height="800px"))
+            .add(
+            "",
+            nodes_data,
+            links_data,
+            repulsion=8000,
+            edge_label=opts.LabelOpts(
+                is_show=True, position="middle", formatter="{c}"
+            )
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title="james_hive_lineage_parser")
+        )
+            .render("james_hive_lineage_parser.html")
+    )
+
+
+def main():
+    DB_HOST = CO['LOCAL_DB']['host']
+    DB_USER = CO['LOCAL_DB']['user']
+    DB_PASSWD = CO['LOCAL_DB']['passwd']
+    DB_DB = CO['LOCAL_DB']['db']
+    DB_PORT = CO['LOCAL_DB'].as_int('port')
+
+    DB_CONN = pymysql.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD,
+                              db=DB_DB,
+                              port=DB_PORT,
+                              charset='utf8')
+
+    DB_COR = DB_CONN.cursor()
+    results = read_from_table(DB_COR)
+    lineage = results[0][0]
+    # print(f"{lineage}")
+    # print('-'*160)
+    process_lineage_hook_info(lineage)
+
+
+if __name__ == '__main__':
+    main()
