9e6e276914f3e4f9b88a5cc8b27214c293375871
james <james@JamesUbuntu>
2020-11-14 21:29:21 +0800

    add james_baidu_map

---
+++ james_http/james_baidu_map.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+def parse_location_with_baidu_api(lat_lon):
+    baidu_map_url_prefix = "http://api.map.baidu.com/reverse_geocoding/v3/?ak=t0cHujK5IFBwzt9IUsZkFgdHhCicOB15&output=json&coordtype=wgs84ll&location="
+
+    req_url = f"{baidu_map_url_prefix}{lat_lon}"
+    print(f"reqt_url={req_url}")
+
+    resp_json=requests.get(req_url).json()
+    print(resp_json.get("result").get("formatted_address"))
+
+
+def main():
+    lat_lon = "31.225696563611,121.49884033194"
+    parse_location_with_baidu_api(lat_lon)
+
+
+if __name__ == "__main__":
+    main()
